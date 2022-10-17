84522390d7c19ee13ba8d0039011c83b3410c819
ZenMakerLabInc <63379059+ZenMakerLabInc@users.noreply.github.com>
2022-10-17 11:28:51 -0700

    added test command for extension

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
-})
+namespace microbitBot {
+    /**
+     * Bot goes forward by inputted distance in cm
+     */
+    //% blockId = microbitbotforward
+    //% block="forward by"
+    //% distance.min=0 distance.max=100
+    export function forward(distance: number) : void {
+        const run_time = distance * 500;
+        basic.showString("HELLO!" + run_time);
+    }
+}
