654acedf6515e29a1acb8a149f578c20ee6d281e
ZenMakerLabInc <63379059+ZenMakerLabInc@users.noreply.github.com>
2022-10-18 12:17:12 -0700

    fixed extension weights

--- main.ts
+++ main.ts
@@ -1,13 +1,14 @@
 /**
  * MicrobitBot Module
  */
-//% weight=100 color=#F38020 icon="\uf0eb" block="MicrobitBot"
+//% weight=20 color=#F38020 block="MicrobitBot"
 namespace microbitBot {
+
     /**
      * Bot goes forward by inputted distance in cm
      */
-    //% blockId = "MicrobitBot_forward" block="forward by $distance"
-    //% weight = 100
+    //% block="forward by $distance"
+    //% weight=100
     //% distance.min=0 distance.max=100
     export function forward(distance: number): void {
         const run_time = distance * 500;
@@ -21,9 +22,9 @@ namespace microbitBot {
      * Bot will turn left (counterclockwise) by inputted angle; is a relative angle to current
      * position
      */
-    //% blockId = "MicrobitBot_turnLeft" block = "turn left by $angle"
-    //% weight = 30
-    //% angle.min = 0 angle.max = 360
+    //% block="turn left by $angle"
+    //% weight=30
+    //% angle.min=0 angle.max=360
     export function turnLeft(angle: number): void {
         //TODO: ideally add code to make the bot turn by a certain amount
         basic.showString("LEFT" + angle);
@@ -36,9 +37,9 @@ namespace microbitBot {
      * Bot will turn right (clockwise) by inputted angle; is a relative angle to current
      * position
      */
-    //% blockId = "MicrobitBot_turnRight" block = "turn right by $angle"
-    //% angle.min = 0 angle.max = 360
-    //% weight = 10
+    //% block="turn right by $angle"
+    //% angle.min=0 angle.max=360
+    //% weight=10
     export function turnRight(angle: number): void {
         //TODO: ideally add code to make the bot turn by a certain amount
         basic.showString("RIGHT" + angle);
